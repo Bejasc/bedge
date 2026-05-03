68f183a829c9b6ce29cf4c55ef7e90a2f440abf7
Bejasc <benjamin.schulz94@gmail.com>
2026-05-03 22:35:54 +0930

    chore(bot): remove duplicate lib/database.ts (superseded by @bedge/database)

--- apps/bot/src/lib/database.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-export async function connectDatabase(uri: string): Promise<void> {
-  await mongoose.connect(uri, { serverSelectionTimeoutMS: 5000 });
-}
-
-export function disconnectDatabase(): Promise<void> {
-  return mongoose.disconnect();
-}
